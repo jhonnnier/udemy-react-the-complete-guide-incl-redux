31e22b24c1c503edce279300f5df866d563c9a07
Jhonnier Sánchez <jhsanchez@conexia.com>
2025-07-31 16:39:19 -0500

    Sección 3 - Video 43

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
+import reactImg from './assets/react_001.jpg'
+
+const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
+
+function genRandomInt(max) {
+    return Math.floor(Math.random() * (max + 1));
+}
+
 function Header() {
+    const description = reactDescriptions[genRandomInt(2)];
+
     return (
         <header>
-            <img src="src/assets/react.svg" alt="Stylized atom"/>
+            <img src={reactImg} alt="Stylized atom"/>
             <h1>React Essentials</h1>
             <p>
-                Fundamental React concepts you will need for almost any app you are
-                going to build!
+                {description} React concepts you will need for almost any app you are going to build!
             </p>
         </header>
     );
