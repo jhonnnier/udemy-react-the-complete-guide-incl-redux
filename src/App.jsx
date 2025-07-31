113629af0bbbca54c58bcc7f356ea1f2987e4110
Jhonnier Sánchez <jhsanchez@conexia.com>
2025-07-31 17:20:50 -0500

    Sección 3 - Video 44 - 45 - 46 - Props - Props con desestructuración

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
-import reactImg from './assets/react_001.jpg'
+import reactImg from './assets/react-core-concepts.png'
+import imgComponent from './assets/components.png'
+import {CORE_CONCEPTS} from "./data.js";
 
 const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
 
 function genRandomInt(max) {
     return Math.floor(Math.random() * (max + 1));
 }
+
 
 function Header() {
     const description = reactDescriptions[genRandomInt(2)];
@@ -20,12 +23,43 @@ function Header() {
     );
 }
 
+function CoreConcept(props) {
+    return (
+        <li>
+            <img src={props.image} alt="..."/>
+            <h3>{props.title} </h3>
+            <p>{props.description} </p>
+        </li>
+    );
+}
+
+function CoreConceptDestructure({image, title, description}) {
+    return (
+        <li>
+            <img src={image} alt="..."/>
+            <h3>{title} </h3>
+            <p>{description}</p>
+        </li>
+    );
+}
+
 function App() {
     return (
         <div>
             <Header/>
             <main>
-                <h2>Time to get started!</h2>
+                <section id="core-concepts">
+                    <h2>Core Concepts</h2>
+                    <ul>
+                        <CoreConcept title={CORE_CONCEPTS[0].title} 
+                                     description={CORE_CONCEPTS[0].title} 
+                                     image={CORE_CONCEPTS[0].image}/>
+
+                        <CoreConcept {...CORE_CONCEPTS[1]}/>
+                        <CoreConceptDestructure {...CORE_CONCEPTS[2]}/>
+                        <CoreConceptDestructure {...CORE_CONCEPTS[3]}/>
+                    </ul>
+                </section>
             </main>
         </div>
     );
